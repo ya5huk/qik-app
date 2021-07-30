1d1a70185b3dd1d510330f3e6eb97c9b59a2afae
Ilan Yashuk <ilan147963@gmail.com>
2021-07-30 18:32:14 +0300

    Db pass change

--- lib/Database.ts
+++ lib/Database.ts
@@ -1,9 +1,9 @@
 import { Db, FindOptions, MongoClient, ObjectId } from "mongodb";
 import { Post } from "./Interfaces";
 
-// Creating global database:
-let client = new MongoClient(process.env.DB_URI as string)
+const client = new MongoClient(process.env.DB_URI as string);
 const clientPromise = client.connect();
+
 
 export const connectMongoDB = async () => {
   try {
@@ -14,10 +14,7 @@ export const connectMongoDB = async () => {
   }
 };
 
-export const addToCollection = async (
-  collection: string,
-  addedData: any
-) => {
+export const addToCollection = async (collection: string, addedData: any) => {
   const client = await clientPromise;
   // Add the user to database!
   const db = client.db();
@@ -39,38 +36,46 @@ export const findUserById = async (id: string) => {
   const client = await clientPromise;
   const db = client.db();
   const usersCollection = db.collection("Users");
-  return await usersCollection.findOne({_id: new ObjectId(id)});
-}
+  return await usersCollection.findOne({ _id: new ObjectId(id) });
+};
 
 export const getPosts = async (bundleNum: number) => {
   const client = await clientPromise;
   const db = client.db();
   const usersCollection = db.collection("Posts");
-  return usersCollection.find().skip(10 * bundleNum).limit(10);
-}
+  return usersCollection
+    .find()
+    .skip(10 * bundleNum)
+    .limit(10);
+};
 
 export const changePostLikeAmount = async (post: Post, amount: number) => {
   const client = await clientPromise;
   const db = client.db();
   const usersCollection = db.collection("Posts");
-  const newPost: Post = {...post, likesAmount: post.likesAmount + amount}; // Changing likes here
-  const res = await usersCollection.findOneAndReplace({_id: new ObjectId(post.id)}, newPost);
+  const newPost: Post = { ...post, likesAmount: post.likesAmount + amount }; // Changing likes here
+  const res = await usersCollection.findOneAndReplace(
+    { _id: new ObjectId(post.id) },
+    newPost
+  );
   return res;
-} 
+};
 
 export const getAllPostsId = async () => {
   const client = await clientPromise;
   const db = client.db();
   const usersCollection = db.collection("Posts");
-  const res = await usersCollection.find({}, {projection: {_id: 1}}).toArray();
-  const userId = res.map(u => u._id.toString())
+  const res = await usersCollection
+    .find({}, { projection: { _id: 1 } })
+    .toArray();
+  const userId = res.map((u) => u._id.toString());
   return userId;
-}
+};
 
-export const getPostById = async( id: string) => {
+export const getPostById = async (id: string) => {
   const client = await clientPromise;
   const db = client.db();
   const usersCollection = db.collection("Posts");
-  const res = await usersCollection.findOne({_id: new ObjectId(id)});
+  const res = await usersCollection.findOne({ _id: new ObjectId(id) });
   return res;
-}
+};

eeed6126fedf3c0176a6b6d140337db88a28e8ab
Ilan Yashuk <ilan147963@gmail.com>
2021-08-08 14:13:06 +0300

    Added logout/profile click on navbar

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -1,19 +1,31 @@
-import classes from './Navbar.module.css';
-import React from 'react';
+import classes from "./Navbar.module.css";
+import React from "react";
+import router from 'next/router';
 
 const Navbar: React.FC = () => {
-    return <div className={`${classes.navContainer} myFont`}>
-    <nav className="d-flex px-5 pb-3 align-items-center justify-content-between">
-      <div className="left">
-        <a className={`mt-auto ${classes.navLink}`}>profile</a>
-      
-      </div>
-      <div className="right">
-        <a className={`mt-auto ${classes.navLink}`}>sign out</a>
-      </div>
+  const handleLogout = (e: any) => {
+    e.preventDefault();
+    localStorage.removeItem('?');
+    router.push('/');
+  }
 
-    </nav>
-  </div>
-}
+  const handleProfileClick = (e: any) => {
+    e.preventDefault();
+    const userId = localStorage.getItem('?') as string;
+    router.push(`/accounts/${userId}`);
+  }
+  return (
+    <div className={`${classes.navContainer} myFont`}>
+      <nav className="d-flex px-5 pb-3 align-items-center justify-content-between">
+        <div className="left">
+          <a onClick={handleProfileClick} className={`mt-auto ${classes.navLink}`}>profile</a>
+        </div>
+        <div className="right">
+          <a onClick={handleLogout} className={`mt-auto ${classes.navLink}`}>sign out</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
